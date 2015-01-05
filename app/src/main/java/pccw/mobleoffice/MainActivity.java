cd34154f4011a392ccf9b4d357a913e9550596f3
jameskaron <jameskaron@126.com>
2015-01-05 18:15:54 +0800

    123

--- app/src/main/java/pccw/mobleoffice/MainActivity.java
+++ app/src/main/java/pccw/mobleoffice/MainActivity.java
@@ -1,17 +1,32 @@
 package pccw.mobleoffice;
 
+import android.app.AlertDialog;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private ImageButton button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button3 = (ImageButton) findViewById(R.id.button3);
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "clicked", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 
